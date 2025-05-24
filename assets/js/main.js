43278066b70c647ac3c2d7d65d999b12d80da93c
Alaa Mekibes <alaamks115@gmail.com>
2025-05-24 21:37:02 +0100

    first upload

---
+++ assets/js/main.js
@@ -0,0 +1,52 @@
+class inputVerification {
+    constructor() {
+        this.selectedbtn = document.querySelectorAll(".choosing .numbers button");
+        this.submit = document.querySelector(".submit button");
+        this.selected = false;
+        this.numberSelected;
+    }
+    chooseBtn() {
+        this.selectedbtn.forEach(el => {
+            el.addEventListener("click", _ => {
+                this.selectedbtn.forEach(btn => {btn.classList.remove("selected"); btn.setAttribute("aria-checked", "false");});
+                el.classList.add("selected");
+                el.setAttribute("aria-checked", "true");
+                this.selected = true;
+                this.numberSelected = el.textContent;
+            })});
+    }
+    submitNumber() {
+        this.submit.addEventListener("click", _ => {
+            if(this.selected === false) {
+                document.querySelector(".error").textContent = "You must select one number!";
+                document.querySelector(".error").setAttribute("aria-live", "assertive");
+                this.errorTimer();
+                return;
+            }
+            else {
+                document.querySelector(".container").innerHTML = `
+            <div class="thanks" aria-hidden="true">
+                <img src="assets/images/illustration-thank-you.svg" alt="">
+                </div>
+                <p class="whatSelected">You selected ${this.numberSelected} out of 5</p>
+            <section>
+            <h1 class="thanks">Thank you!</h1>
+            <p class="message">We appreciate you taking the time to give a rating. If you ever need more support, 
+                don't hesitate to get in touch!</p>
+            </section>
+                `;
+                document.querySelector(".container").style.cssText = "text-align: center; animation: fade-in 1s";
+            }
+        })
+    }
+    errorTimer() {
+        setTimeout(() => {
+            document.querySelector(".error").textContent = "";
+        }, 3000);
+    }
+}
+
+// main
+inp = new inputVerification();
+inp.chooseBtn();
+inp.submitNumber();
